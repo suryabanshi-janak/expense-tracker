d84b49e28d234e510be7b53967cc3bf3bb8d4ca1
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-09-14 09:30:21 +0545

    supabase typescript imlementation

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,5 +1,11 @@
 // import * as React from 'react';
 // import { supabase } from '@/config/supabase';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 
 import { Button, buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
@@ -30,6 +36,31 @@ export default function Category() {
         >
           Create a Category
         </Link>
+      </div>
+
+      <div>
+        <Accordion type='single' collapsible className='w-full'>
+          <AccordionItem value='item-1'>
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value='item-2'>
+            <AccordionTrigger>Is it styled?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It comes with default styles that matches the other
+              components&apos; aesthetic.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value='item-3'>
+            <AccordionTrigger>Is it animated?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It&apos;s animated by default, but you can disable it if you
+              prefer.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
     </>
   );
